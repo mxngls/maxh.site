1887c586abdadf1e31a173d3ff426bdde570a245
mxngls <Maximilian.e.hoenig@gmail.com>
2025-07-17 10:00:27 +0900

    Allow passing more input variables to Make

--- make_macos.sh
+++ make_macos.sh
@@ -4,4 +4,4 @@
 export MallocNanoZone=0
 
 # overwrite defaults set for FreeBSD when working locally on macOS
-make SYSTEM_LIBS='-lz -liconv -framework CoreFoundation -framework Security'
+make SYSTEM_LIBS='-lz -liconv -framework CoreFoundation -framework Security' "$@"
